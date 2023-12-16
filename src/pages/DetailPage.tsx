cf3f4f4b5854e6256a1ef89ff5b0b098701201de
federicanicoloits <127236809+federicanicoloits@users.noreply.github.com>
2023-12-16 10:16:37 +0100

    COLORE SVG

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -16,8 +16,8 @@ const DetailPage = () => {
       <div className="flex items-center justify-center h-[100vh] w-[100wv]">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
           <path
-            fill="#FF156D"
-            stroke="#FF156D"
+            fill="#FFFFFF"
+            stroke="#FFFFFF"
             strokeWidth="15"
             transform-origin="center"
             d="m148 84.7 13.8-8-10-17.3-13.8 8a50 50 0 0 0-27.4-15.9v-16h-20v16A50 50 0 0 0 63 67.4l-13.8-8-10 17.3 13.8 8a50 50 0 0 0 0 31.7l-13.8 8 10 17.3 13.8-8a50 50 0 0 0 27.5 15.9v16h20v-16a50 50 0 0 0 27.4-15.9l13.8 8 10-17.3-13.8-8a50 50 0 0 0 0-31.7Zm-47.5 50.8a35 35 0 1 1 0-70 35 35 0 0 1 0 70Z"
